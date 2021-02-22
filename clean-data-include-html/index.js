17870393eb864888d68aec33693159efa8d3caac
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2021-02-22 19:32:20 +0700

    cleaned data / bug (wrong logic) :bug:

--- clean-data-include-html/index.js
+++ clean-data-include-html/index.js
@@ -6,11 +6,11 @@ const striptags = require("striptags");
 const hostname = "127.0.0.1";
 const port = 3003;
 
-const listDataFromDb = [];
-let newData;
+const newData = [];
+const queryList = [];
 
 let db = new sqlite3.Database(
-  "./db/english-vocab-html-raw.db",
+  "./db/english-vocab-html-clean.db",
   sqlite3.OPEN_READWRITE,
   (err) => {
     if (err) {
@@ -20,27 +20,36 @@ let db = new sqlite3.Database(
   }
 );
 
-db.serialize(() => {
-  db.each(`SELECT word as w, html_raw as raw FROM vi_word_copy`, (err, row) => {
-    if (err) {
-      console.error(err.message);
+db.all("SELECT word, html_raw FROM vi_word_copy", {},
+  (error, rows) => {
+    if (error) {
+      console.error(error.message);
     }
-    let data = {
-      word: row.w,
-      html_raw: row.raw,
-    };
-    listDataFromDb.push(data);
-    handleData();
-  });
-});
 
-const handleData = () => {
-  newData = listDataFromDb.map((item, index) => {
-    if (index > 100) {
-      return cleanData(item);
-    }
+    db.serialize(function () {
+      rows.forEach((row, index) => {
+        let item = {
+          word: row.word,
+          html_raw: row.html_raw,
+        };
+        const itemAfterClean = cleanData(item)
+        // console.log(`item: ${JSON.stringify(itemAfterClean)} \n\n\n`);
+        const itemQuery = `UPDATE vi_word_copy SET example = '${JSON.stringify(itemAfterClean.example)}' WHERE word LIKE "${itemAfterClean.word}"`
+        // console.log(">>>>>>: ", itemQuery)
+
+        db.run(itemQuery, function (err) {
+          if (err) {
+            console.log(">>>:     ", itemAfterClean.example);
+            console.log(err);
+            console.log(">>>:     ", itemQuery);
+          } else {
+            console.log("Success !!!");
+          }
+        })
+      })
+
+    })
   });
-};
 
 // clearn data make them become an object
 const cleanData = (item) => {
@@ -51,36 +60,48 @@ const cleanData = (item) => {
   const cleanString = striptags(`${item.html_raw}`);
   const formatString = cleanString !== "null" ? cleanString : "";
 
+  // format raw data to multiple string like
+  /**
+   * 
+   * line 1
+   * line 2
+   * line 3
+   */
+
   var lines = formatString.split("\n");
-  // console.log("lines.length >>>", lines.length);
+
+  // first line in lines is always empty
   if (lines.length === 1) {
-    // console.log(">>> ");
-    return "";
+    data = {
+      ...data,
+      example: ""
+    };
+    return data;
   }
+
+  // if lines is not empty
+  // create new object
+  // with new key is "ex[i]"
   if (lines.length > 1) {
+    const ex = {}
     for (i = 1; i < lines.length; i++) {
       if (i < lines.length - 1) {
+        ex[i] = lines[i].replace(/\'/g, "''");
         data = {
           ...data,
-          ex: {
-            ex1: lines[1],
-            ex2: lines[2],
-            ex3: lines[3],
-            ex4: lines[4],
-            ex5: lines[5],
-          },
+          example: ex
         };
       }
     }
+
     return data;
   }
 };
 
 const server = http.createServer((req, res) => {
-  console.log("SIZE: ", newData.length);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("hello world".toString());
+  res.end(newData.length.toString());
 });
 
 server.listen(port, hostname, () => {
